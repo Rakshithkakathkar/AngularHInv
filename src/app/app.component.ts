0aadd549311ad19f75591a52feda04f09cd0f228
Rakshith kakathkar <rakshithkakathkar@gmail.com>
2022-08-31 23:57:57 +0530

    content-projection, AfterContentInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,21 @@ import { RoomsComponent } from './rooms/rooms.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
+
+export class AppComponent{
+// export class AppComponent implements AfterViewInit {
   title = 'hotelinventoryapp';
   role = 'Admin';
 
   // dynamic rendering of rooms component.
   // ViewContainerRef is used to dynamically load a component
-  @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
+  // @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
 
   // since we are using viewChild by default static is false
-  ngAfterViewInit(): void {
-    const componentRef = this.vcr.createComponent(RoomsComponent);
+  // ngAfterViewInit(): void {
+  //   const componentRef = this.vcr.createComponent(RoomsComponent);
 
     // using componentRef we can now access properties of RoomsComponent
-    componentRef.instance.numberOfRooms = 50;
-  }
+  //   componentRef.instance.numberOfRooms = 50;
+  // }
 }
---
+++ src/app/container/container.component.ts
@@ -0,0 +1,24 @@
+import { AfterContentInit, Component, ContentChild, OnInit } from '@angular/core';
+import { EmployeeComponent } from '../employee/employee.component';
+
+@Component({
+  selector: 'hinv-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.scss'],
+})
+export class ContainerComponent implements OnInit, AfterContentInit {
+  
+  // used to access the content which is in ng-content
+  @ContentChild(EmployeeComponent) employee! : EmployeeComponent;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // this lifecycle hook can be used only when view are loading components using ng-component
+  // we can access the properties of those components from here
+
+  ngAfterContentInit(): void {
+    // we are modifying the proerties of employeeComponent
+    this.employee.empName = 'Raj';
+  }
+}
