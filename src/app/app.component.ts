7a21237b4b560bc539a1379be934b3cdda449e71
Rakshith kakathkar <rakshithkakathkar@gmail.com>
2022-08-31 23:40:07 +0530

    dynamic rendering of component using ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'hinv-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'hotelinventoryapp';
   role = 'Admin';
+
+  // dynamic rendering of rooms component.
+  // ViewContainerRef is used to dynamically load a component
+  @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
+
+  // since we are using viewChild by default static is false
+  ngAfterViewInit(): void {
+    const componentRef = this.vcr.createComponent(RoomsComponent);
+
+    // using componentRef we can now access properties of RoomsComponent
+    componentRef.instance.numberOfRooms = 50;
+  }
 }
